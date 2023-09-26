2b864e5b6d63f03ea24582f4aadf47ab537d453b
OzaRu <114357633+0zaRu@users.noreply.github.com>
2023-09-27 01:33:03 +0200

    Add files via upload

---
+++ prueba/src/main/java/com/mycompany/prueba/Prueba.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author 6002754
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        Ventana v = new Ventana();
+        v.setVisible(true);
+    }
+}
